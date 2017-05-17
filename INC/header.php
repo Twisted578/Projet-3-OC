f023c89b19f0e0cffb835e69231053dda2058173
Guénolé <guenole578@gmail.com>
2017-05-17 17:44:14 +0200

    Création de l'index et de l'include header

---
+++ INC/header.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="fr"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+  <title>Billet simple pour l'Alaska</title>
+  <meta name="description" content="">
+
+  <meta name="viewport" content="width=device-width">
+
+  <!-- Place favicon.ico and apple-touch-icon.png in the root directory: mathiasbynens.be/notes/touch-icons -->
+
+  <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="css/main.css">
+<!-- nivo slider  -->
+  <link rel="stylesheet" href="js/nivo-slider/themes/default/default.css" type="text/css" media="screen" />
+ <link rel="stylesheet" href="js/nivo-slider/nivo-slider.css" type="text/css" media="screen" />
+</head>
---
+++ index.php
@@ -0,0 +1,150 @@
+<?php  
+include 'INC/header.php'; 
+
+?>
+
+<!-- PRESENTATION -->
+<div id="presentation">
+	<div  class="container presentation clearfix">		     
+		<!-- SLIDER -->
+		<div class="slider-wrapper theme-default">				
+				  <div id="slider" class="nivoSlider">				
+											
+						<a href="">
+							<img src="img/alaska1.jpg" alt="" /></a>
+					
+											
+						<a href="">
+							<img src="img/alaska2.jpg" alt=""/></a>
+					
+											
+						<a href="">
+							<img  src="img/alaska3.jpg" alt=""  /></a>
+
+				</div>
+		</div>
+	</div> <!-- container-->
+</div> <!-- presentation -->
+
+<!-- PAGE -->
+<div id="page">
+    <div id ="contenuPage" class="container clearfix">	
+			<div class="accueil">
+				<h2>Bienvenue,</h2>
+				<p>
+                    Bonjour, je m'appelle Jean Forteroche et je suis acteur et écrivain. J'écris essentiellement des livres qui parlent de mes voyages en tant qu'acteur et comme vous pouvez le constater, j'ai tourné mon dernier film en Alaska dont le titre de mon livre.
+                </p>
+                <p>
+                Cela fait 20 ans que je fais ces deux métier et aujourd'hui j'ai envie de vous faire partager mon expérience à travers un livre totalement en ligne qui je l'espère plairont au plus grand nombres d'entre vous. 
+                </p>
+                <h3>
+                	<strong>Je vous laisse donc suivre mon incroyable voyage et bonne lecture !</strong>
+                </h3>
+                </br>
+                </br>
+			</div>
+			<section class="lastArticles boxArticles">
+				<h3>Derniers articles </h3>
+				<ul>
+					<li>
+						<a href="">
+							<div class="thumb"><img src="img/photo1.png" alt ="" /></div>
+							<div class="detail">
+								<h4>Final FAntasy XIII</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua.  </p>
+							</div>
+						</a>
+					</li>
+					<li>
+						<div class="thumb"><img src="img/photo1.png" alt ="" /></div>
+						<div class="detail">
+							<h4>Final FAntasy XIII</h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua.  </p>
+						</div>
+					</li>
+					<li>
+						<div class="thumb"><img src="img/photo1.png" alt ="" /></div>
+						<div class="detail">
+							<h4>Final FAntasy XIII</h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua.  </p>
+						</div>
+					</li>
+					<li>
+						<div class="thumb"><img src="img/photo1.png" alt ="" /></div>
+						<div class="detail">
+							<h4>Final FAntasy XIII</h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua.  </p>
+						</div>
+					</li>
+					<li>
+						<div class="thumb"><img src="img/photo1.png" alt ="" /></div>
+						<div class="detail">
+							<h4>Final FAntasy XIII</h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua.  </p>
+						</div>
+					</li>
+					<li>
+						<div class="thumb"><img src="img/photo1.png" alt ="" /></div>
+						<div class="detail">
+							<h4>Final FAntasy XIII</h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua.  </p>
+						</div>
+					</li>
+				</ul>
+			</section>
+
+			<section class="lastComments boxComments">
+				<h3>Derniers commentaires</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. </p>
+				<p>Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure </p>
+				<p>dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. </p>
+				<p>Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure </p>
+			</section>
+			<div class="clearfix"></div>
+			<div class="accueil">
+				<h3>A propos</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+			
+
+	</div> <!-- contenuPage -->
+</div> <!-- #page -->
+
+<!-- FOOTER -->
+<footer>
+	<div class=" container clearfix">
+		<p>Projet crée et développé par <a href="https://www.facebook.com/guenole.lequentrec">Guénolé Le Quentrec</a> </p>
+		<p>&copy; Athakim - 2013</p>			
+	</div> 
+</footer>
+
+</div> 
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/jquery-1.7.1.min.js"><\/script>')</script>
+  <script type="text/javascript" src="js/nivo-slider/jquery.nivo.slider.js"></script>
+    <script type="text/javascript">
+    $(window).load(function() {
+        $('#slider').nivoSlider();
+    });
+    </script>
+</body>
+</html>
