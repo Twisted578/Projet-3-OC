36bc7e5d1f4747d1ee89d04be232633d9bdc067d
Guénolé <guenole578@gmail.com>
2017-05-18 16:40:38 +0200

    connexion à la db donc création des fichiers db.php, config.php ainsi que includes.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-include 'INC/header.php' 
+
+include 'INC/includes.php';
+
+include 'INC/header.php';
 
 ini_set('display_errors',1); 
 ?>
@@ -133,18 +136,6 @@ ini_set('display_errors',1);
 
 <?php 
 
-include 'INC/footer.php'
+include 'INC/footer.php';
 
 ?>
-</div> 
-
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="js/jquery-1.7.1.min.js"><\/script>')</script>
-  <script type="text/javascript" src="js/nivo-slider/jquery.nivo.slider.js"></script>
-    <script type="text/javascript">
-    $(window).load(function() {
-        $('#slider').nivoSlider();
-    });
-    </script>
-</body>
-</html>
